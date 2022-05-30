43ca8f3c2558150b3419384d670538496255b3f6
YUJIN <yujinj704@gmail.com>
2022-05-30 11:45:17 +0900

    board #1

---
+++ mysite02/src/main/java/com/douzone/mysite/controller/BoardActionFactory.java
@@ -0,0 +1,13 @@
+package com.douzone.mysite.controller;
+
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
+
+public class BoardActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		return null;
+	}
+
+}
