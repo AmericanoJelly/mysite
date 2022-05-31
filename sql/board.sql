57f1ba7971df8f6ca287a3ce328fd0b24659a983
YUJIN <yujinj704@gmail.com>
2022-05-31 11:02:35 +0900

    sql

--- sql/board.sql
+++ sql/board.sql
@@ -3,12 +3,19 @@ desc board;
 select * from board;
 
 
- insert into board values ( 1, "test01", "안녕하세요 홀홀홀", 33, now(), 1, 1, 1, 1);
-  insert into board values ( 2, "test02", "testtesteteste", 22, now(), 2, 1, 1, 1);
+ insert into board values ( null, "test01", "안녕하세요 홀홀홀", 33, now(), 1, 1, 1, 1);
+  insert into board values ( null, "test02", "testtesteteste", 22, now(), 2, 1, 1, 1);
  
  select a.title, b.name, a.hit, a.reg_date
  from board a, user b
  where a.user_no = b.no
-order by a.g_no desc, a.o_no asc;
+ and a.user_no = 2;
+ 
  
 select title, contents from board;
+
+insert into board values 
+( null, "test03", "test03333333333333", 0, now(),(select max(g_no)from board a)+1 , 1, 1, 2);
+        
+        
+select max(g_no)from board ;
